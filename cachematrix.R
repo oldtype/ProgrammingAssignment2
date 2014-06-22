87707ddfe9b5b1a44007c240fc42d76a7092fc7d
oldtype <i.oldtype@gmail.com>
2014-06-23 07:00:05 +0900

    comments added for makeCacheMatrix() and cacheSolve()

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,9 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## makeCacheMatrix() mananges a object which is a cache 
+## preserving a calculated value of the inverse matrix
+## given by argument x (x must be a matrix)
 makeCacheMatrix <- function(x = matrix()) {
   inv <- NULL
   set <- function(y) {
@@ -17,7 +19,12 @@ makeCacheMatrix <- function(x = matrix()) {
   
 }
 
-## Write a short comment describing this function
+## cacheSolve() gets a makeCacheMatrix object given by
+## argument x then checks whether it has cached value
+## of inverse matrix. It a cache exists, then 
+## cacheSolve() just returns it.  Otherwise, cacheSolve()
+## calculates the inverse matrix, stores its value to the 
+## cache of the makeCacheMatrix object.
 cacheSolve <- function(x, ...) {
   ## Return a matrix that is the inverse of 'x'
   inv <- x$getInverse()
